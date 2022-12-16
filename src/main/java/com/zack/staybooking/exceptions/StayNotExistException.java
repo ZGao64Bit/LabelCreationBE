f4e22a63e54e58688c75ad17d5de7f00ffab4254
Zack Gao <zichunkami@gmail.com>
2022-12-16 18:16:26 -0500

    Final

--- src/main/java/com/zack/staybooking/exceptions/StayNotExistException.java
+++
@@ -1,7 +0,0 @@
-package com.zack.staybooking.exceptions;
-
-public class StayNotExistException extends RuntimeException {
-    public StayNotExistException(String msg) {
-        super(msg);
-    }
-}
